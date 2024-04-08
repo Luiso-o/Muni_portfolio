ff07de58056c63778e459f0b2042f48600491813
Luiso-o <cheportillo@gmail.com>
2024-04-08 08:43:12 +0200

    mejoras

--- js/header.js
+++ js/header.js
@@ -1,3 +1,4 @@
+//logica para desaparecer y  aparecer la barra de menus
 let ultimaPosicionScroll = 0;
 
 window.addEventListener("scroll", function() {
@@ -18,3 +19,27 @@ window.addEventListener("scroll", function() {
     ultimaPosicionScroll = posicionActualScroll <= 0 ? 0 : posicionActualScroll;
 });
 
+//toggler
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const iconBars = navbarToggler.querySelector('.fa-bars');
+    const iconTimes = navbarToggler.querySelector('.fa-times');
+    const navbarMenu = document.querySelector('#navbarNav');
+
+    navbarToggler.addEventListener('click', function() {
+        iconBars.style.display = iconBars.style.display === 'none' ? '' : 'none';
+        iconTimes.style.display = iconTimes.style.display === 'none' ? '' : 'none';
+
+        navbarMenu.classList.toggle('blur-effect');
+    });
+});
+
+
+
+  
+  
+  
+  
+  
+  
+  
