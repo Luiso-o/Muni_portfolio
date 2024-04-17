5a8f5d6a0f7aaa0ae320986dee3c50e970922683
Luiso-o <cheportillo@gmail.com>
2024-04-17 20:26:17 +0200

    cambios grandes

--- js/galleryManager.js
+++ js/galleryManager.js
@@ -1,30 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     fetch('resources/images.json')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) throw new Error('Network response was not ok.');
+            return response.json();
+        })
         .then(data => {
             const images = data.images;
             const columns = 4;
             const imagesPerColumn = Math.ceil(images.length / columns);
             const imageRow = document.getElementById('imageRow');
+            const fragment = document.createDocumentFragment();
 
             for (let i = 0; i < columns; i++) {
                 let columnDiv = document.createElement('div');
                 columnDiv.className = 'column';
                 for (let j = 0; j < imagesPerColumn; j++) {
                     let imgIndex = i * imagesPerColumn + j;
-                    if (imgIndex < images.length) {
-                        let imgElement = document.createElement('img');
-                        imgElement.src = `gallery/${images[imgIndex]}`;
-                        imgElement.loading = 'lazy';
-                        imgElement.className = 'img-fluid';
-                        imgElement.alt = 'Image not available';
-                        columnDiv.appendChild(imgElement);
-                    }
+                    if (imgIndex >= images.length) break;
+                    let imgElement = document.createElement('img');
+                    imgElement.src = `gallery/${images[imgIndex]}`;
+                    imgElement.loading = 'lazy';
+                    imgElement.className = 'img-fluid';
+                    imgElement.alt = 'Image not available';
+                    columnDiv.appendChild(imgElement);
                 }
-                imageRow.appendChild(columnDiv);
+                fragment.appendChild(columnDiv);
             }
+            imageRow.appendChild(fragment);
             initializeModalListeners();
-        });
+        })
+        .catch(error => console.error('Error fetching the images:', error));
 });
 
 //modal
@@ -32,39 +37,36 @@ function initializeModalListeners() {
     const images = document.querySelectorAll(".column img");
     const modal = document.getElementById("myModal");
     const modalImg = document.getElementById("imgModal");
-    const navbar = document.querySelector('.navbar');
+    const header = document.getElementById('header');
+    let currentIndex = 0;
 
     images.forEach((img, index) => {
         img.addEventListener('click', () => {
             modal.style.display = "flex";
             modalImg.src = img.src;
-            navbar.style.display = "none";
+            header.style.display = "none";
+            currentIndex = index;
         });
     });
 
     const close = document.querySelector('.close');
-    close.addEventListener('click', () => {
-        modal.style.display = "none";
-        navbar.style.display = "flex";
-    });
+    close.addEventListener('click', () => modalClose());
 
     window.addEventListener('click', (event) => {
-        if (event.target === modal) {
-            modal.style.display = "none";
-            navbar.style.display = "flex";
-        }
+        if (event.target === modal) modalClose();
     });
 
     document.addEventListener('keydown', (event) => {
-        if (event.key === "Escape") {
-            modal.style.display = "none";
-            navbar.style.display = "flex";
-        }
+        if (event.key === "Escape") modalClose();
     });
+
+    function modalClose() {
+        modal.style.display = "none";
+        header.style.display = "block"; 
+    }
 
     const prevButton = document.querySelector('.prev');
     const nextButton = document.querySelector('.next');
-    let currentIndex = 0;
 
     prevButton.addEventListener('click', () => {
         currentIndex = (currentIndex + images.length - 1) % images.length;
