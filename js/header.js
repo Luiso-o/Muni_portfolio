677478e3d06d736c6138174949aabf60aa278b03
Luiso-o <cheportillo@gmail.com>
2024-04-19 12:40:25 +0200

    about

--- js/header.js
+++ js/header.js
@@ -37,5 +37,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-
+$(document).ready(function(){
+    // Mostrar el dropdown al pasar el mouse
+    $('.dropdown').hover(function(){
+      $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+      $(this).toggleClass('open');        
+    },
+    function(){
+      $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+      $(this).toggleClass('open');       
+    });
+  });
 
