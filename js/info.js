75be90b3755b909d0272809fda9e8c2a23f1bb0c
Luiso-o <cheportillo@gmail.com>
2024-04-24 13:44:36 +0200

    brandings

--- js/info.js
+++ js/info.js
@@ -1,11 +1,13 @@
 document.addEventListener('scroll', function() {
-    const parallax = document.querySelector('.parallax');
-    if (parallax) {
-      const offset = window.scrollY;
-      const speed = 0.3;
-      const yPos = -offset * speed;
-  
-      parallax.style.backgroundPosition = `center ${yPos}px`;
-    }
-  });
-  
+  // Solo ejecuta el parallax si el ancho de la ventana es mayor a 768px
+  if (window.innerWidth > 768) {
+      const parallax = document.querySelector('.parallax');
+      if (parallax) {
+          const offset = window.scrollY;
+          const speed = 0.3;
+          const yPos = -offset * speed;
+
+          parallax.style.backgroundPosition = `center ${yPos}px`;
+      }
+  }
+});
